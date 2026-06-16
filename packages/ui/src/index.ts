0f069a8ec5c3aa41958a04ab09ee4ef8f4d49cbc
JakeIRL <145846954+jslawson865@users.noreply.github.com>
2026-06-16 04:26:01 -0400

    Stage UI token package

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,6 @@
+export const MYFACE_THEME_TOKENS = {
+  founder: "olympus-retro-founder",
+  default: "retro-social-default",
+  danger: "moderation-warning",
+  trusted: "trusted-node"
+} as const;
